971aad54c04f8d377b29afca63b222e8f74f4cd3
Rebecca Garcia <shelteredheart21@gmail.com>
2022-08-09 15:02:32 -0400

    done

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -14,6 +14,18 @@
 
 /**
  * @type {Cypress.PluginConfig}
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  */
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
