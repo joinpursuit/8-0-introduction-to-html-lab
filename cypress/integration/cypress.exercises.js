2190c2b883e99f82ac7619644a027d8780bf3360
Victor Rey <victorrey@pursuit.org>
2022-08-28 11:23:11 -0400

    Finished

---
+++ cypress/integration/cypress.exercises.js
@@ -0,0 +1,12 @@
+describe("get some practice in", () => {
+    beforeEach(() => {
+        // We want to visit https://www.york.ac.uk/teaching/cws/wws/webpage1.html for each test
+    })
+
+    it("has an H1 tag that contains STARTING", () => {});
+
+    it("has exactly 17 p tags on the page", () => {});
+
+    // Note, you might have to google how to check what URL you're on
+    it("goes to the next lesson link when you click on the lesson two link", () => {});
+})
